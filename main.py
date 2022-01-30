df4a3195401fc9bc409581cfaef4875022ca2471
Lapwell <76802132+Lapwell@users.noreply.github.com>
2022-01-29 18:16:11 -0800

    Forgo the last update, were going pygame from the get go. Working on movement right now.

--- main.py
+++ main.py
@@ -1,54 +1,55 @@
 import pygame
 from sys import exit
 
-pygame.init()  # Not sure why, but this is REQUIRED to be run before any other pygame code, even though, without it, it runs fine. Do as those before say, at least in the beginning...
+pygame.init()  # Not sure why, but this is REQUIRED to be run before any other pygame code even though, without it, it runs fine. Do as those before say, at least in the beginning...
 
 # All constant values
-FPS = 60
-WIDTH, HEIGHT = 512, 512
+FPS = 30
+WIDTH, HEIGHT = 500, 500
 ROOT = pygame.display.set_mode((WIDTH, HEIGHT))
 RED = (255, 32, 32)
 GREEN = (32, 255, 32)
 BLUE = (32, 32, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+VEL = 5
 
-grid = []  # Grid will be used to make the game world and store the player's position
+
 clock = pygame.time.Clock()  # I don't know what it's called when I do this, I'll call it "assigning functions." I think it's an apt description. Anyway, assigning funcs goes here.
-body = pygame.Rect(10, 10, 24, 24)  # This is stuff for the snak's body
+test_rect = pygame.Rect(200, 200, 10, 10)
+x, y = 100, 100              # The starting pos of the snake
 
 
 # This function does as its name describes, it updates the window when called so things that happen behind the scenes (like a ship moving) actually shows up.
 def draw_root():
-	ROOT.fill(WHITE)
-	pygame.draw.rect(ROOT, GREEN, body)
-	pygame.display.update()
+    ROOT.fill(BLACK)
+    pygame.draw.rect(ROOT, GREEN, test_rect)
+    pygame.display.update()
 
 
-# This function is for the user to set the number of row and columns for the game.
-def start_menu():
-	ROOT.fill(BLACK)
-
+def check_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # If the event pygame.QUIT is called (in this case by pressing the exit button) it stops the loop.
+            pygame.quit()
+            exit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        test_rect.y -= VEL
+    if keys[pygame.K_DOWN]:
+        test_rect.y += VEL
+    if keys[pygame.K_LEFT]:
+        test_rect.x -= VEL
+    if keys[pygame.K_RIGHT]:
+        test_rect.x += VEL
 
 
 # This is the main game function, rather obvious based on its name.
 def main():
-
-	start_menu()
-	print('test 1')
-	start_menu_true = True
-	while start_menu_true:
-		start_menu()
-
-	while True:
-
-		clock.tick(FPS)  # This limits the game so it runs at 60FPS.
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:  # If the event pygame.QUIT is called (in this case by pressing the exit button) it stops the loop.
-				pygame.quit()
-				exit()
-		draw_root()
+    while True:
+        check_events()
+        draw_root()
+        clock.tick(FPS)  # This limits the game, so it runs at the set FPS.
 
 
 if __name__ == '__main__':
-	main()
+    main()
